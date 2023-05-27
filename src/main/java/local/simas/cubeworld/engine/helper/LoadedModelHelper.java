df9ef4fe585b6bd06f4a1959a5d485d47924bddb
Linas Nikiperavičius <linas@linasdev.com>
2023-05-27 13:38:48 +0300

    Fix LoadedModelHelper

--- src/main/java/local/simas/cubeworld/engine/helper/LoadedModelHelper.java
+++ src/main/java/local/simas/cubeworld/engine/helper/LoadedModelHelper.java
@@ -1,26 +1,51 @@
 package local.simas.cubeworld.engine.helper;
 
 import local.simas.cubeworld.engine.data.LoadedModel;
+import local.simas.cubeworld.engine.data.LoadedTexture;
+import local.simas.cubeworld.engine.data.RawModel;
+import local.simas.cubeworld.engine.data.TexturedModel;
+import local.simas.cubeworld.engine.loader.ModelLoader;
+import local.simas.cubeworld.engine.loader.RawModelLoader;
+import local.simas.cubeworld.engine.loader.TextureLoader;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class LoadedModelHelper {
-    private static Map<Long, LoadedModel> loadedModelMap = new HashMap<>();
+    private static RawModelLoader rawModelLoader = new RawModelLoader();
+    private static ModelLoader modelLoader = new ModelLoader();
+    private static TextureLoader textureLoader = new TextureLoader();
+    private static Map<Long, TexturedModel> texturedModelMap = new HashMap<>();
 
-    public static void setLoadedModelForType(Long type, LoadedModel loadedModel) {
-        if (loadedModelMap.containsKey(type)) {
-            throw new IllegalArgumentException(String.format("Loaded model overriding is not allowed, fix double-setting of type '%d'", type));
+    public static void setTexturedModelForType(Long type, TexturedModel texturedModel) {
+        if (texturedModelMap.containsKey(type)) {
+            throw new IllegalArgumentException(String.format("Textured model overriding is not allowed, fix double-setting of type '%d'", type));
         }
 
-        loadedModelMap.put(type, loadedModel);
+        texturedModelMap.put(type, texturedModel);
     }
 
-    public static LoadedModel getLoadedModelForType(Long type) {
-        if (!loadedModelMap.containsKey(type)) {
-            throw new IllegalArgumentException(String.format("Loaded model is not present in the map, set loaded model of type '%d'", type));
+    public static void loadLoadedModelForType(Long type, String modelPath, String texturePath, float reflectivity, float shineDamper) throws IOException {
+        RawModel rawModel = rawModelLoader.loadRawModelFromFile(modelPath);
+        LoadedModel loadedModel = modelLoader.loadRawModel(rawModel);
+
+        LoadedTexture loadedTexture = textureLoader.loadTextureFromFile(texturePath, reflectivity, shineDamper);
+
+        setTexturedModelForType(
+                type,
+                TexturedModel.builder()
+                        .model(loadedModel)
+                        .texture(loadedTexture)
+                        .build()
+        );
+    }
+
+    public static TexturedModel getTexturedModelForType(Long type) {
+        if (!texturedModelMap.containsKey(type)) {
+            throw new IllegalArgumentException(String.format("Textured model is not present in the map, set or load textured model of type '%d'", type));
         }
 
-        return loadedModelMap.get(type);
+        return texturedModelMap.get(type);
     }
 }
